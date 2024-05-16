013e4985640089cd6f1fe06e181f42ffb9a48d3d
dusk_peyl <aztrocord@gmail.com>
2024-05-16 23:02:50 +1000

    fixed an issue where test file would not run due to being unable to import pytest

--- terminal_application_script.sh
+++ terminal_application_script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-cd ./src
 
-if [[ -x "$(command -v python)" ]]
+if [[ -x "$(command Python3 --version)" ]]
 then
-    python_version="$(python --version)" 
-    if [[ $python_version == "Python 3"* ]]
+    python_version="$(python -version 2>&1)" 
+    if [[ $python_version == "Python3"* ]]
     then
         python3 -m venv .venv
-        source .venv/bin/activate
+        source ./.venv/bin/activate
+        cd ./src
         pip3 install -r ./requirements.txt
         python3 main.py
     else
-        echo "Your version of Python is too old. Please update it."
+        echo "Your version of Python is too old. Please update it." >&2
     fi
 else
     echo 'Error!
     It looks like Python is not installed! This program requires Python to run!
-    To install Python, check out https://www.python.org/downloads/'
+    To install Python, check out https://www.python.org/downloads/' >&2
     exit 1
 fi
